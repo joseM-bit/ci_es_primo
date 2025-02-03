098f8e217b2f1afe24c706460a6bb464358a7661
joseM-bit <122779053+joseM-bit@users.noreply.github.com>
2025-02-03 09:12:27 +0100

    Add files via upload

---
+++ proves/test_arc/test_funcios.py
@@ -0,0 +1,13 @@
+from src.funcions import es_primo
+
+def test_primo_1():
+    assert es_primo(1) == False
+    
+def test_es_primo_numero_primo():
+    assert es_primo(2) == True
+def test_es_primo_negativo():
+    assert es_primo(-10) == False
+def test_es_primo_numero_primo_mayor_2():
+    assert es_primo(29) == True
+def test_es_primo_numero_no_primo():
+    assert es_primo(4) == False
